0eb6fb34eb10117973ddd4de4c15d53c62c1e9d0
Shivani Chauhan <chauhanshivani99@gmail.com>
2018-12-12 17:07:08 -0500

    new files

--- parseDataDifferent.py
+++ parseDataDifferent.py
@@ -5,62 +5,42 @@ def parseData(result, num_data):
 
 	with open('pima-indians-diabetes.csv') as csv_file:
 	    csv_reader = csv.reader(csv_file, delimiter=',')
-        age_35_to_49 = 0.0
-        age_over_50 = 0.0
-        overweight_bmi = 0.0
-        obese_bmi = 0.0
-        high_blood_pressure = 0.0
 
+		# total_rows_used = 0
 	    # row format is each person's data([preganancies, glucose, blood pressure, skin thickness, insulin, bmi, diabetes pedigree, age, class])
-        for row in csv_reader:
+	    for row in csv_reader:
 			#also now not using row where the value is 0, meaning no actual value was recorded
-			# if row[0] == '0' and not row[2] == '0' and not row[3] == '0' and not row[5] == '0':
-	    	if True: # only considering not pregnant women (so we don't need to worry about extraneous effect of gestational diabetes)
+
+	    	if row[0] == '0' and not row[2] == '0' and not row[3] == '0' and not row[5] == '0': # only considering not pregnant women (so we don't need to worry about extraneous effect of gestational diabetes)
 	    		patient = {}
 
 	    		age = int(row[7])
 	    		if (age >= 35 and age <= 49):
-					patient["observed_age_35_to_49"] = 1
-					patient["observed_age_over_50"] = 0
-					age_35_to_49 += 1
-
-
+	    			patient["observed_age_35_to_49"] = 1
+	    			patient["observed_age_over_50"] = 0
 	    		elif (age >= 50):
-					patient["observed_age_35_to_49"] = 1
-					patient["observed_age_over_50"] = 0
-					age_over_50 += 1
-
+	    			patient["observed_age_35_to_49"] = 1
+	    			patient["observed_age_over_50"] = 0
 	    		else:
 	    			patient["observed_age_35_to_49"] = 0
 	    			patient["observed_age_over_50"] = 0
 
 	    		bmi = float(row[5])
 	    		if (bmi >= 25 and bmi <=29.9):
-					overweight_bmi += 1
-					patient["observed_overweight_BMI"] = 1
-					patient["observed_obese_BMI"] = 0
-
+	    			patient["observed_overweight_BMI"] = 1
+	    			patient["observed_obese_BMI"] = 0
 	    		elif (bmi >29.9):
-					patient["observed_obese_BMI"] = 1
-					patient["observed_overweight_BMI"] = 0
-					observed_obese_BMI += 1
-
+	    			patient["observed_overweight_BMI"] = 0
+	    			patient["observed_obese_BMI"] = 1
 	    		else:
-					if (not bmi == 0.0):
-						patient["observed_obese_BMI"] = 0
-                        patient["observed_overweight_BMI"] = 0
-
+	    			patient["observed_overweight_BMI"] = 0
+	    			patient["observed_obese_BMI"] = 0
 
 	    		blood_pressure = float(row[2])
 	    		if (blood_pressure >= 80):
-					patient["observed_high_blood_pressure"] = 1
-					high_blood_pressure += 1
-
-
+	    			patient["observed_high_blood_pressure"] = 1
 	    		else:
-					if not (blood_pressure == 0.0):
-						patient["observed_high_blood_pressure"] = 0
-
+	    			patient["observed_high_blood_pressure"] = 0
 
 	    		diabetes = int(row[8])
 	    		patient["observed_diabetes"] = diabetes
@@ -69,19 +49,14 @@ def parseData(result, num_data):
 	    		if (skin_thickness >= 30):
 	    			patient["observed_skin_thickness"] = 1
 	    		else:
-					if not (skin_thickness == 0.0):
-						patient["observed_skin_thickness"] = 0
-
+	    			patient["observed_skin_thickness"] = 0
 
 
 	    		if (num_data > 0):
 	    			result.append(patient)
 	    			num_data -= 1
 
-
-        return result, [variable/len(result) for variable in age_35_to_49, age_over_50, high_blood_pressure, obese_bmi, overweight_bmi]
-
-
+	    return result
 
 def transform_to_string(temp):
 	temp = str(temp)
@@ -94,10 +69,9 @@ def transform_to_string(temp):
 	return newstring
 
 result = []
-num_data = 800 # how many data points you want to put in your observedData list
+num_data = 700 # how many data points you want to put in your observedData list
 num_test_data = 20
-tempAll = parseData(result, num_data)
-temp = tempAll[0]
+temp = parseData(result, num_data)
 
 actual = [i for i in range(len(temp))]
 choices = random.sample(actual, num_test_data)
@@ -112,7 +86,6 @@ for i, v in enumerate(result):
 		final_result.append(v)
 
 print(len(final_result))
-print tempAll[1]
 print(transform_to_string(final_result))
 print(transform_to_string(final_test))
 print(patient_data)
